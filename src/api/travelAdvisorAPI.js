9c54a7550150a2024c2a2a0d739b4d2fc3372d04
Felipe Castillo <34404062+felipecastillo-b@users.noreply.github.com>
2022-04-12 02:36:24 -0400

    final changes

--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -11,7 +11,7 @@ export const getPlacesData = async (type, sw, ne) => {
         tr_latitude: ne.lat,
       },
       headers: {
-        'x-rapidapi-key': '7eefae993dmsh49e7c50a05a5090p16aa15jsn3eb6b27f0066',
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
         'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
       },
     });
@@ -28,7 +28,7 @@ export const getWeatherData = async (lat, lng) => {
       const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
         params: { lat, lon: lng },
         headers: {
-          'x-rapidapi-key': '7eefae993dmsh49e7c50a05a5090p16aa15jsn3eb6b27f0066',
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
           'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
         },
       });
